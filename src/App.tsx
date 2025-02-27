5a1aaa1e1b43b43aabd7b517dcaa99238f1288e2
Marius Høy <marius.hoy@gmail.com>
2025-02-27 14:43:02 +0100

    Added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import {Button} from "@/components/ui/button.tsx";
+
 export default function App() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
             <h1 className="text-4xl font-bold text-blue-600">Stockpile Manager</h1>
             <p className="text-gray-700 mt-2">Tailwind CSS is working! 🎉</p>
+            <Button>Click me</Button>
         </div>
     );
 }
